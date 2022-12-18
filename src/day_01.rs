c8d3ad40acef8652e3030ceb8c73661657ec1c89
Gernot Raudner <gernot.raudner@dynatrace.com>
2022-12-18 15:52:45 +0000

    day 01 complete

---
+++ src/day_01.rs
@@ -0,0 +1,23 @@
+use std::fs;
+
+pub fn day_01() {
+    let (final_floor, cellar_index) = walk_floors("./input/day_01.txt");
+    println!("Santa reached floor {}", final_floor);
+    println!("Reached floor at {}", cellar_index + 1);
+}
+
+fn walk_floors(path: &str) -> (i32, usize) {
+    let input = fs::read_to_string(path).unwrap();
+    let (up, down, cellar_index) = input.chars().enumerate().fold((0, 0, usize::MAX), |(mut up, mut down, mut cellar_index), (index, ch)| {
+        match ch {
+            ')' => down += 1,
+            '(' => up += 1,
+            _ => panic!("Unknown character: {ch}")
+        };
+        if cellar_index == usize::MAX && down > up {
+            cellar_index = index;
+        }
+        (up, down, cellar_index)
+    });
+    (up - down, cellar_index)
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use day_01::day_01;
+
+mod day_01;
+
+fn main() {
+    day_01();
+}
